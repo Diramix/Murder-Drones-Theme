439a65f2f394c5f161dd260288096ac884ff18b1
Diramix <danyagranny10@proton.me>
2026-02-23 21:11:03 +0300

    fix: titlebar text

--- Murder Drones/script.js
+++ Murder Drones/script.js
@@ -2,10 +2,13 @@
 const body = document.body;
 
 const applyTheme = () => {
-    if (!body.classList.contains('ym-dark-theme') && !body.classList.contains('ym-light-theme')) {
-        body.classList.add('ym-dark-theme');
-    } else if (body.classList.contains('ym-light-theme')) {
-        body.classList.replace('ym-light-theme', 'ym-dark-theme');
+    if (
+        !body.classList.contains("ym-dark-theme") &&
+        !body.classList.contains("ym-light-theme")
+    ) {
+        body.classList.add("ym-dark-theme");
+    } else if (body.classList.contains("ym-light-theme")) {
+        body.classList.replace("ym-light-theme", "ym-dark-theme");
     }
 };
 
@@ -13,4 +16,21 @@ applyTheme();
 
 const observer = new MutationObserver(() => applyTheme());
 
-observer.observe(body, { attributes: true, attributeFilter: ['class'] });
+observer.observe(body, { attributes: true, attributeFilter: ["class"] });
+
+// PulseText remover
+const pulseTextObserver = new MutationObserver(() => {
+    document.querySelectorAll('[class*="TitleBar_pulseText"]').forEach((el) => {
+        el.classList.forEach((cls) => {
+            if (cls.includes("TitleBar_pulseText")) {
+                el.classList.replace(cls, "biteMeText");
+                el.textContent = `BITE ME!`;
+            }
+        });
+    });
+});
+
+pulseTextObserver.observe(document.body, {
+    childList: true,
+    subtree: true,
+});
